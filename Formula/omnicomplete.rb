4e17f5c9cacce4a203bbcd7e34ecfa4a273224f1
Lirim Krosa <krosalirim@gmail.com>
2026-07-23 11:50:59 +0200

    Initial commit of omnicomplete Homebrew formula

---
+++ Formula/omnicomplete.rb
@@ -0,0 +1,21 @@
+require "language/node"
+
+class Omnicomplete < Formula
+  desc "A blazingly fast, native autocomplete UI for Zsh"
+  homepage "https://github.com/lirimkrosa/omnicomplete"
+  # This URL points to the NPM registry tarball, which gets published when you run `npm publish`
+  url "https://registry.npmjs.org/omnicomplete/-/omnicomplete-1.0.0.tgz"
+  # You will need to update the sha256 when you publish a new version
+  sha256 "REPLACE_WITH_SHA256"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/omni", "--version"
+  end
+end
