43125b01ea436c194f62b3ec2522649b82f99bcd
Lirim Krosa <krosalirim@gmail.com>
2026-07-23 12:14:07 +0200

    Bump to 1.0.2 with updated README

--- Formula/omnicomplete.rb
+++ Formula/omnicomplete.rb
@@ -4,9 +4,9 @@ class Omnicomplete < Formula
   desc "A blazingly fast, native autocomplete UI for Zsh"
   homepage "https://github.com/lirimkrosa/omnicomplete"
   # This URL points to the NPM registry tarball, which gets published when you run `npm publish`
-  url "https://registry.npmjs.org/@lirimkrosa/omnicomplete/-/omnicomplete-1.0.1.tgz"
+  url "https://registry.npmjs.org/@lirimkrosa/omnicomplete/-/omnicomplete-1.0.2.tgz"
   # You will need to update the sha256 when you publish a new version
-  sha256 "7026fbfb289a6d0a231d2cbc015887120fa4b8fe229fc0b9dfdd041333e31a09"
+  sha256 "e73adafce3ebea1a48480707e94bc2a3d780e255d2d3dcc1e02191922155972a"
 
   depends_on "node"
 
